42b7d182ac24ed1331bc8d978d6647f0198590c9
RemiW <remi.willems59@gmail.com>
2018-03-01 11:51:49 +0100

    Database

--- app/src/main/java/com/example/quentin/moviesappisen/MainActivity.java
+++ app/src/main/java/com/example/quentin/moviesappisen/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.quentin.moviesappisen;
 
+import android.arch.persistence.room.Room;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.quentin.moviesappisen.database.AppDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        AppDatabase db = Room.databaseBuilder(getApplicationContext(),
+                AppDatabase.class, "misenadb").build();
+
         setContentView(R.layout.activity_main);
     }
 }
